a895590964d23b7cecc79b37d40723f7530ce9a8
Noah <nochen@dep.nyc.gov>
2021-11-25 14:56:16 -0500

    Test moving Navigation Bar elements to separate component

---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,83 @@
+//import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
+import MUIDeleteModal from './MUIDeleteModal'
+
+import HomeIcon from '@mui/icons-material/Home'
+import GroupsIcon from '@mui/icons-material/Groups'
+import PersonIcon from '@mui/icons-material/Person'
+import FunctionsIcon from '@mui/icons-material/Functions'
+import Fab from '@mui/material/Fab'
+//import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField';
+
+function NavigationBar () {
+    function handleHomePairs() {
+        setSearchState("Home");
+        store.homeTest();
+    }
+    function handleAllListsPairs() {
+        setSearchState("All Lists");
+        store.getAllLists();
+    }
+    function handleUserPairs() {
+        //console.log(document.getElementById("search-bar").value);
+        setSearchState("Users");
+        store.getUsersLists();
+    }
+    //NEED SOMETHING FOR COMMUNITY LISTS IDK
+    function search() {
+        let criteria = document.getElementById("search-bar").value;
+        console.log(searchState + ": " + criteria);
+        if (searchState === "Home") {
+            store.searchHomePairsByName(criteria);
+            //console.log(store.idNamePairs);
+        }
+        else if (searchState === "All Lists") {
+            store.searchAllListsByName(criteria);
+        }
+        else if (searchState === "Users") {
+            //FILL THIS IN AND PROBABLY REPLACE WITH SWITCH???
+            store.searchUsersListsByUser(criteria);
+        }
+    }
+    return (
+        <div id="top5-list-selector" style={{backgroundColor: '#c4c4c4'}}>
+            <div id="list-selector-heading">
+            <Fab 
+                color="primary" 
+                aria-label="add"
+                onClick={handleHomePairs}
+            >
+                <HomeIcon />
+            </Fab>
+            <Fab 
+                color="primary" 
+                aria-label="add"
+                onClick={handleAllListsPairs}
+            >
+                <GroupsIcon />
+            </Fab>
+            <Fab 
+                color="primary" 
+                aria-label="add"
+                onClick={handleUserPairs}
+            >
+                <PersonIcon />
+            </Fab>
+            <Fab 
+                color="primary" 
+                aria-label="add"
+                onClick={handleHomePairs}
+            >
+                <FunctionsIcon />
+            </Fab>
+            <TextField id='search-bar' placeholder="Search" type="search" onKeyDown={(e) => {if (e.key === "Enter") { search(); }}} />
+            </div>
+            <div id="list-selector-list">
+                {
+                    listCard
+                }
+                <MUIDeleteModal />
+            </div>
+        </div>)
+}
